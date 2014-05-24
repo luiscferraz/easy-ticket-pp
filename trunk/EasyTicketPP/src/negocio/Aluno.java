cd653398bc077e6fd53544e2a3dfe8ef761b58f1
nandacms@gmail.com <nandacms@gmail.com@913f3ca0-620b-0106-3180-ee12275b9e00>
2014-05-24 19:16:38 +0000

    Criação da classe Cargo. Modificação nas classes Aluno,Funcionário e Pessoa.

--- trunk/EasyTicketPP/src/negocio/Aluno.java
+++ trunk/EasyTicketPP/src/negocio/Aluno.java
@@ -1,5 +1,40 @@
 package negocio;
 
-public class Aluno {
+import java.sql.Date;
+
+public class Aluno extends Pessoa{
+	
+	private int idCurso;
+
+	public Aluno(int id, String nome, String email, String cpf,
+			String telefone, Date dataNascimento, String status,int idCurso) {
+		super(id, nome, email, cpf, telefone, dataNascimento, status);
+		this.idCurso=idCurso;		
+	}
+
+	public int getIdCurso() {
+		return idCurso;
+	}
+
+	public void setIdCurso(int idCurso) {
+		this.idCurso = idCurso;
+	}
+
+	
+	
+	
+    
+	
+        
+	
+	
+	
+
+	
+	
+	
+	
+    
+    
 
 }
